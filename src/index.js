600656cac05fb13aac47fddf28cc7dfb8cf9d58d
Thomas de Lachaux <thomas.delachaux@gmail.com>
2020-05-26 17:27:48 +0200

    Remove fake hosts

--- src/index.js
+++ src/index.js
@@ -4,29 +4,9 @@ const { fetchHosts, startPingInterval } = require('./utils/ping');
 const { initApi } = require('./api');
 const log = require('./utils/log');
 
-// list des ip
-const hosts = [
-  {
-    name: 'google-dns',
-    ip: '8.8.8.8',
-  },
-  {
-    name: '42',
-    ip: '163.172.250.12',
-  },
-  {
-    name: 'stationf',
-    ip: '104.26.13.60',
-  },
-  {
-    name: 'lol',
-    ip: '192.168.1.214',
-  },
-];
-
 (async () => {
   try {
-    // const hosts = await fetchHosts();
+    const hosts = await fetchHosts();
     const database = await initDatabase();
 
     startPingInterval(hosts, database);
